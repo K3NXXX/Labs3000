4b44069623084ce9cc05a8884e47fe4ae4364be9
Andrushevskiy Volodymyr <116910392+K3NXXX@users.noreply.github.com>
2022-10-29 11:59:36 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,20 +1,22 @@
+x = "e"
+while x == "e":
+    num1 = int(input("Enter first number: "))
+    num2 = input("Choose action:")
+    num3 = int(input("Enter second number: "))
 
-num1 = int(input("Enter first number: "))
-num2 = input("Choose action:")
-num3 = int(input("Enter second number: "))
-
-if num2 == "+":
-    print("Result : ", num1 + num3)
-elif num2 == "-":
-    print("Result : ", num1 - num3)
-elif num2 == "*":
-    print("Result : ", num1 * num3)
-elif num2 == "**":
-    print("Result : ", num1 ** num3)
-elif num2 == "/" and num3 == 0:
-    print("Бліч топ")
-elif num2 == "/":
-    print("Result : ", num1 / num3)
+    if num2 == "+":
+        print("Result : ", num1 + num3)
+    elif num2 == "-":
+        print("Result : ", num1 - num3)
+    elif num2 == "*":
+        print("Result : ", num1 * num3)
+    elif num2 == "**":
+        print("Result : ", num1 ** num3)
+    elif num2 == "/" and num3 == 0:
+        print("Бліч топ")
+    elif num2 == "/":
+        print("Result : ", num1 / num3)
+    x = input("Press 'e' to continue ")
 
 
 
